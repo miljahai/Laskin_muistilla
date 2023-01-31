90bea302a9d69992c4b970fe5bfee08305a802fb
unknown <milja.haiko@gmail.com>
2023-01-31 10:53:16 +0200

    tehtävä kesken

--- App.js
+++ App.js
@@ -1,12 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [luku1, setluku1] = useState('');
+  const [luku2, setluku2] = useState('');
+
+  const [vastaus, setvastaus] = useState('');
+  const [data, setData] = useState([]);
+
+  const initialFocus = useRef(null);
+
+  const calculate = operator => {
+    const [number1, number2] = [Number(luku1), Number(luku2)];
+  
+  switch (operator) {
+    case '+':
+      setvastaus(number1+number2);
+      break;
+
+    case '-':
+      setvastaus(number1-number2);
+      break;
+  }
+  setluku1('');
+  setluku2('');
+  initialFocus.current.focus();
+}
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Result: {vastaus}</Text>
+
+      <TextInput style={styles.input} ref={initialFocus} keyboardType='numeric'  onChangeText={text => setluku1(text)} value={luku1}/>
+      <TextInput style={styles.input} keyboardType='numeric'  onChangeText={text => setluku2(text)} value={luku2}/>
+        <View style={styles.operators}>
+          <View style={styles.button}>
+            <Button onPress={() => calculate('+')} title="+" />
+            </View>
+            <View style={styles.button}>
+            <Button onPress={() => calculate('-')} title="-" />
+          </View>
+          <View>
+            <FlatList data={data} renderItem={({item}) => <Text>{item.key}</Text>}
+              keyExtractor={(item, index) => index.toString()} />
+          </View>
+        </View>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -17,4 +61,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    width:200  , 
+    borderColor: 'gray', 
+    borderWidth: 1
+  },
+  operators: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  button : {
+    width: '10%',
+    margin: 10     
+  },
+
 });
